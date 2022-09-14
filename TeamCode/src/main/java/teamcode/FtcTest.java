0ddec33cdd67e78823f3135d2b2e6454a2f77726
trc492 <trc492@live.com>
2022-09-14 04:05:53 -0700

    Added Calibrate Swerve Steering as a test choice.

--- TeamCode/src/main/java/teamcode/FtcTest.java
+++ TeamCode/src/main/java/teamcode/FtcTest.java
@@ -733,6 +733,7 @@ public class FtcTest extends FtcTeleOp
         testMenu.addChoice("Tune Y PID", Test.TUNE_Y_PID, false, tuneKpMenu);
         testMenu.addChoice("Tune Turn PID", Test.TUNE_TURN_PID, false, tuneKpMenu);
         testMenu.addChoice("Pure Pursuit Drive", Test.PURE_PURSUIT_DRIVE, false);
+        testMenu.addChoice("Calibrate Swerve Steering", Test.CALIBRATE_SWERVE_STEERING, false);
 
         xTargetMenu.setChildMenu(yTargetMenu);
         yTargetMenu.setChildMenu(turnTargetMenu);
