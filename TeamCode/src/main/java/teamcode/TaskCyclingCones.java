3015b9360cd0e917c353625d7210cf85119d0754
trc492 <trc492@live.com>
2022-10-23 01:41:50 -0700

    Code reviewed TaskCyclingCones.

--- TeamCode/src/main/java/teamcode/TaskCyclingCones.java
+++ TeamCode/src/main/java/teamcode/TaskCyclingCones.java
@@ -22,8 +22,6 @@
 
 package teamcode;
 
-import org.checkerframework.checker.units.qual.C;
-
 import TrcCommonLib.trclib.TrcDbgTrace;
 import TrcCommonLib.trclib.TrcEvent;
 import TrcCommonLib.trclib.TrcNotifier;
@@ -49,15 +47,15 @@ public class TaskCyclingCones
 
     public enum CycleType
     {
-        AUTO_FULL_CYCLE, //does a full cycle(pickup and scoring), used for auto
-        SCORING_ONLY,//assumes robot already has a cone, scores it onto the pole
-        PICKUP_ONLY//picks up a cone from the stack
+        AUTO_FULL_CYCLE,    //does a full cycle(pickup and scoring), used for auto
+        SCORING_ONLY,       //assumes robot already has a cone, scores it onto the pole
+        PICKUP_ONLY         //picks up a cone from the stack
     }
 
     public enum VisionType
     {
         NO_VISION,
-        ALIGN_VISION//uses vision to horizontally align to target
+        ALIGN_VISION        //uses vision to horizontally align to target
     }
 
     /*
@@ -87,20 +85,20 @@ public class TaskCyclingCones
     }
 
     private final Robot robot;
-    private FtcAuto.AutoChoices autoChoices;
     private final TrcStateMachine<State> sm;
-    private final TrcTaskMgr.TaskObject cycleTaskObj;
-    private TrcDbgTrace msgTracer = null;
-    private CycleType cycleType = CycleType.AUTO_FULL_CYCLE;
-    private VisionType visionType = VisionType.ALIGN_VISION;
-    private TrcEvent onFinishEvent = null;
-    private TrcNotifier.Receiver onFinishCallback = null;
     private final TrcEvent driveEvent;
     private final TrcEvent elevatorEvent;
     private final TrcEvent armEvent;
     private final TrcEvent turretEvent;
     private final TrcEvent intakeEvent;
+    private final TrcTaskMgr.TaskObject cycleTaskObj;
+    private TrcDbgTrace msgTracer = null;
+    private FtcAuto.AutoChoices autoChoices;
+    private CycleType cycleType = CycleType.AUTO_FULL_CYCLE;
+    private VisionType visionType = VisionType.ALIGN_VISION;
     private int conesRemaining;
+    private TrcEvent onFinishEvent = null;
+    private TrcNotifier.Receiver onFinishCallback = null;
 
     //location of the cone or pole relative to the robot
     private TrcPose2D targetLocation;
@@ -108,12 +106,12 @@ public class TaskCyclingCones
     public TaskCyclingCones(Robot robot)
     {
         this.robot = robot;
+        sm = new TrcStateMachine<>(moduleName);
         driveEvent = new TrcEvent(moduleName + ".drive");
         elevatorEvent = new TrcEvent(moduleName + ".elevator");
         armEvent = new TrcEvent(moduleName + ".arm");
         turretEvent = new TrcEvent(moduleName + ".turret");
         intakeEvent = new TrcEvent(moduleName + ".intake");
-        sm = new TrcStateMachine<>(moduleName);
         cycleTaskObj = TrcTaskMgr.createTask(moduleName, this::cycleTask);
     }   //TaskCyclingCones
 
@@ -126,6 +124,11 @@ public class TaskCyclingCones
     {
         msgTracer = tracer;
     }   //setMsgTracer
+
+    public void setAutoChoices(FtcAuto.AutoChoices autoChoices)
+    {
+        this.autoChoices = autoChoices;
+    }
 
     public void cancel()
     {
@@ -144,19 +147,27 @@ public class TaskCyclingCones
             onFinishCallback = null;
         }
     }   //cancel
-    public void doFullAutoCycle(VisionType visionType, int conesRemaining, TrcEvent event){
+
+    public void doFullAutoCycle(VisionType visionType, int conesRemaining, TrcEvent event)
+    {
         startCycling(CycleType.AUTO_FULL_CYCLE, visionType, conesRemaining, event, null);
     }
+
     //teleop call to do pickupOnly
-    public void doPickup(VisionType visionType, int conesRemaining, TrcEvent event){
+    public void doPickup(VisionType visionType, int conesRemaining, TrcEvent event)
+    {
         startCycling(CycleType.PICKUP_ONLY, visionType, conesRemaining, event, null);
     }
+
     //teleop call to do scoring, only the cone it currently has
-    public void scoreCone(VisionType visionType, TrcEvent event){
+    public void scoreCone(VisionType visionType, TrcEvent event)
+    {
         startCycling(CycleType.SCORING_ONLY, visionType, 1, event, null);
     }
+
     //prepare for cycling, start sm
-    public void startCycling(CycleType cycleType, VisionType visionType, int conesRemaining, TrcEvent event, TrcNotifier.Receiver callback)
+    public void startCycling(
+        CycleType cycleType, VisionType visionType, int conesRemaining, TrcEvent event, TrcNotifier.Receiver callback)
     {
         this.cycleType = cycleType;
         this.visionType = visionType;
@@ -165,15 +176,11 @@ public class TaskCyclingCones
         this.onFinishCallback = callback;
         cycleTaskObj.registerTask(TrcTaskMgr.TaskType.FAST_POSTPERIODIC_TASK);
         sm.start(State.PREPARE_PICKUP);
-        if(cycleType == CycleType.SCORING_ONLY){
-            //sm.start(cycleType == CycleType.SCORING_ONLY? State.ALIGN_TO_POLE: State.ALIGN_TO_CONE);
-        }
+//        if (cycleType == CycleType.SCORING_ONLY)
+//        {
+//            //sm.start(cycleType == CycleType.SCORING_ONLY? State.ALIGN_TO_POLE: State.ALIGN_TO_CONE);
+//        }
     }   //startCycling
-
-    public void setAutoChoices(FtcAuto.AutoChoices autoChoices)
-    {
-        this.autoChoices = autoChoices;
-    }
 
     private void cycleTask(TrcTaskMgr.TaskType taskType, TrcRobot.RunMode runMode)
     {
@@ -182,34 +189,46 @@ public class TaskCyclingCones
         if (state != null)
         {
             boolean traceState = true;
-            double matchTime = TrcUtil.getModeElapsedTime();
+//            double matchTime = TrcUtil.getModeElapsedTime();
 
             switch (state)
             {
                 case ALIGN_TO_CONE:
+                    // CodeReview: is there an assumption that vision is enabled? If so, where does vision get enabled?
+                    // Also note that calling real time vision may not get immediate result. You may need to create a
+                    // state to wait for valid vision result. In addition, distanceFromCamera is only available if you
+                    // have valid Homography. Homography assumes the camera is mounted fixed but our camera is mounted
+                    // on the elevator. Therefore, the only thing you can count on is horizontalAngle, you won't have
+                    // valid X and Y.
+                    //
                     //don't use vision for horizontally align if its AUTO_FULL_CYCLE
-                        //because location of cone stack is set
-                    if(cycleType != CycleType.AUTO_FULL_CYCLE && visionType != VisionType.NO_VISION){
+                    //because location of cone stack is set
+                    if (cycleType != CycleType.AUTO_FULL_CYCLE && visionType != VisionType.NO_VISION)
+                    {
                         //use vision to find x, y, location of cone relative to robot
                         TrcVisionTargetInfo<?> targetInfo = robot.vision.getBestDetectedTargetInfo(null);
                         //Todo: not sure if this is correct
                         //relative location from camera to cone
-                        targetLocation = new TrcPose2D(targetInfo.distanceFromCamera.x, targetInfo.distanceFromCamera.y, targetInfo.horizontalAngle);
+                        targetLocation = new TrcPose2D(
+                            targetInfo.distanceFromCamera.x, targetInfo.distanceFromCamera.y,
+                            targetInfo.horizontalAngle);
                     }
                     sm.setState(State.PREPARE_PICKUP);
                     break;
+
                 case PREPARE_PICKUP: //1. drive to the cone(using targetLocation) or auto conestack(set location)
-                    if(targetLocation != null){
+                    if (targetLocation != null)
+                    {
                         robot.robotDrive.purePursuitDrive.start(
-                                driveEvent, robot.robotDrive.driveBase.getFieldPosition(), true,
-                                targetLocation);
+                            driveEvent, robot.robotDrive.driveBase.getFieldPosition(), true, targetLocation);
                     }
-                    else if(cycleType == CycleType.AUTO_FULL_CYCLE){
+                    else if(cycleType == CycleType.AUTO_FULL_CYCLE)
+                    {
                         //drive to position where robot can pick up from the cone stack
                         //Todo: find position
                         robot.robotDrive.purePursuitDrive.start(
-                                driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
-                                robot.robotDrive.getAutoTargetPoint(-2.5, -0.3, -104, autoChoices));
+                            driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
+                            robot.robotDrive.getAutoTargetPoint(-2.5, -0.3, -104, autoChoices));
                     }
                     //set elevator to height so that intake is several inches above the cone stack
                     //todo: tune elevator height
@@ -224,6 +243,7 @@ public class TaskCyclingCones
 
                 case START_SPIN: //2. spin turret to the front and spin intake wheels
                     robot.turret.setTarget(RobotParams.TURRET_FRONT, 1.0, turretEvent, null);
+                    // CodeReview: why don't we do autoAssist pickup?
                     robot.intake.setPower(RobotParams.INTAKE_POWER_PICKUP);
                     sm.waitForSingleEvent(turretEvent, State.PICKUP_CONE);
                     break;
@@ -241,8 +261,8 @@ public class TaskCyclingCones
 
                 case PREPARE_SCORE: //5. turn turret to the back while driving backwards to the pole
                     robot.robotDrive.purePursuitDrive.start(
-                            driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
-                            robot.robotDrive.getAutoTargetPoint(-1.2, -0.2, -104.0, autoChoices));
+                        driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
+                        robot.robotDrive.getAutoTargetPoint(-1.2, -0.2, -104.0, autoChoices));
                     robot.turret.setTarget(RobotParams.TURRET_BACK);
                     sm.waitForSingleEvent(driveEvent, State.ALIGN_ARM);
                     break;
@@ -259,8 +279,8 @@ public class TaskCyclingCones
 
                 case RESET: //8. turn turret 180 degrees and lower elevator while driving back to start position
                     robot.robotDrive.purePursuitDrive.start(
-                            driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
-                            robot.robotDrive.getAutoTargetPoint(-1.243, -0.061, -104.0, autoChoices));
+                        driveEvent, robot.robotDrive.driveBase.getFieldPosition(), false,
+                        robot.robotDrive.getAutoTargetPoint(-1.243, -0.061, -104.0, autoChoices));
                     robot.turret.setTarget(RobotParams.TURRET_FRONT, 1.0, turretEvent, null);
                     robot.elevator.setTarget(RobotParams.ELEVATOR_MIN_POS, true, 1.0, elevatorEvent);
                     sm.addEvent(driveEvent);
